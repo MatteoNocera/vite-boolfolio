49273286f373d28ffb610460b51a49d9358e2e93
MatteoNocera <matteo_nocera@tiscali.it>
2023-11-17 19:56:59 +0100

    stamp on page and implement nav and cards

--- src/App.vue
+++ src/App.vue
@@ -34,20 +34,63 @@ export default {
 </script>
 
 <template>
-  <div>ciao</div>
+  <header>
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+      <div class="container">
+        <a class="navbar-brand" href="#">BoolFolio</a>
+        <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse"
+          data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false"
+          aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="collapsibleNavId">
+          <ul class="navbar-nav me-auto mt-2 mt-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" href="#" aria-current="page">Home <span
+                  class="visually-hidden">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="http://127.0.0.1:8000/admin">Admin</a>
+            </li>
+          </ul>
 
-  <section class="projects">
-    <div class="container">
-      <div class="row row-cols-1 row-cols-sm-2 row-cols-lg-4 g-5">
-        <div class="col" v-for="project in projects">
-          <div class="card">
-            <h3>{{ project.title }}</h3>
+        </div>
+      </div>
+    </nav>
 
+  </header>
+  <main class="py-4">
+    <section class="projects">
+      <div class="container">
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-lg-4 g-5">
+          <div class="col" v-for="project in projects">
+            <div class="card h-100">
+              <div class="card-image-top text-center py-2">
+                <img width="100" :src="base_url + '/storage/' + project.cover_image" alt="">
+              </div>
+              <div class="card-body">
+                <div class="card-title">
+                  {{ project.title }}
+                </div>
+                <div class="card-text">
+                  {{ project.description }}
+                </div>
+              </div>
+
+
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </section>
+    </section>
+  </main>
+
+  <footer>
+
+  </footer>
 </template>
 
 <style></style>
