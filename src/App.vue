c6df54303a20f0f6af8966aa73d7723e92966436
MatteoNocera <matteo_nocera@tiscali.it>
2023-11-18 20:12:15 +0100

    add Navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script>
 import axios from 'axios';
 import Card from './components/Card.vue';
+import Navbar from './components/Navbar.vue';
 
 export default {
   name: 'App',
@@ -12,7 +13,8 @@ export default {
     }
   },
   components: {
-    Card
+    Card,
+    Navbar
   },
   methods: {
 
@@ -40,31 +42,7 @@ export default {
 
 <template>
   <header>
-    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
-      <div class="container">
-        <a class="navbar-brand" href="#">BoolFolio</a>
-        <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse"
-          data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false"
-          aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="collapsibleNavId">
-          <ul class="navbar-nav me-auto mt-2 mt-lg-0">
-            <li class="nav-item">
-              <a class="nav-link active" href="#" aria-current="page">Home <span
-                  class="visually-hidden">(current)</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="http://127.0.0.1:8000/admin">Admin</a>
-            </li>
-          </ul>
-
-        </div>
-      </div>
-    </nav>
+    <Navbar></Navbar>
 
   </header>
   <main class="py-4">
