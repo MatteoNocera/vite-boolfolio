b7d50a5ed042ee7727bd002866b15b75b54e22f5
MatteoNocera <matteo_nocera@tiscali.it>
2023-11-20 16:32:52 +0100

    modify card in project list & add vue router

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script>
 import axios from 'axios';
-import Card from './components/Card.vue';
+import ProjectList from './components/ProjectList.vue';
 import Navbar from './components/Navbar.vue';
 
 export default {
@@ -17,36 +17,10 @@ export default {
     }
   },
   components: {
-    Card,
+    ProjectList,
     Navbar
   },
-  methods: {
 
-    getProjects() {
-      /* const url = this.base_url + this.project_api +; */
-
-      axios
-        .get(`${this.base_url}` + `${this.project_api}`, {
-          params: {
-            page: this.currentPage
-          }
-        })
-        .then(response => {
-          console.log(response);
-          this.projects = response.data.result.data;
-          this.currentPage = response.data.result.current_page;
-          this.lastPage = response.data.result.last_page;
-        })
-        .catch(err => {
-          console.error(err);
-        })
-    },
-
-  },
-  mounted() {
-    this.getProjects();
-
-  }
 }
 </script>
 
@@ -55,35 +29,11 @@ export default {
     <Navbar></Navbar>
 
   </header>
+
   <main class="py-4">
-    <section class="projects">
-      <div class="container">
-        <div class="row row-cols-1 row-cols-sm-2 row-cols-lg-4 g-5">
 
-          <Card v-for="project in projects" :project="project"></Card>
+    <ProjectList></ProjectList>
 
-        </div>
-        <div class="my-4">
-          <nav aria-label="Page navigation">
-            <ul class="pagination    ">
-              <li class="page-item disabled">
-                <a class="page-link" href="#" aria-label="Previous">
-                  <span aria-hidden="true">&laquo;</span>
-                </a>
-              </li>
-              <li class="page-item active" aria-current="page"><a class="page-link" href="#">1</a></li>
-              <li class="page-item"><a class="page-link" href="#">2</a></li>
-              <li class="page-item"><a class="page-link" href="#">3</a></li>
-              <li class="page-item">
-                <a class="page-link" href="#" aria-label="Next">
-                  <span aria-hidden="true">&raquo;</span>
-                </a>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </div>
-    </section>
   </main>
 
   <footer>
