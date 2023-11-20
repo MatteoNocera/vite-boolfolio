e676e2c4398a14db2f0174882597411a816a24bf
MatteoNocera <matteo_nocera@tiscali.it>
2023-11-20 16:11:37 +0100

    try to implement pagination

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,11 @@ export default {
     return {
       base_url: 'http://127.0.0.1:8000',
       project_api: '/api/projects',
+
       projects: [],
+
+      currentPage: '',
+      lastPage: '',
     }
   },
   components: {
@@ -19,13 +23,19 @@ export default {
   methods: {
 
     getProjects() {
-      const url = this.base_url + this.project_api;
+      /* const url = this.base_url + this.project_api +; */
 
       axios
-        .get(url)
+        .get(`${this.base_url}` + `${this.project_api}`, {
+          params: {
+            page: this.currentPage
+          }
+        })
         .then(response => {
           console.log(response);
-          this.projects = response.data.result;
+          this.projects = response.data.result.data;
+          this.currentPage = response.data.result.current_page;
+          this.lastPage = response.data.result.last_page;
         })
         .catch(err => {
           console.error(err);
@@ -50,10 +60,27 @@ export default {
       <div class="container">
         <div class="row row-cols-1 row-cols-sm-2 row-cols-lg-4 g-5">
 
-
           <Card v-for="project in projects" :project="project"></Card>
 
-
+        </div>
+        <div class="my-4">
+          <nav aria-label="Page navigation">
+            <ul class="pagination    ">
+              <li class="page-item disabled">
+                <a class="page-link" href="#" aria-label="Previous">
+                  <span aria-hidden="true">&laquo;</span>
+                </a>
+              </li>
+              <li class="page-item active" aria-current="page"><a class="page-link" href="#">1</a></li>
+              <li class="page-item"><a class="page-link" href="#">2</a></li>
+              <li class="page-item"><a class="page-link" href="#">3</a></li>
+              <li class="page-item">
+                <a class="page-link" href="#" aria-label="Next">
+                  <span aria-hidden="true">&raquo;</span>
+                </a>
+              </li>
+            </ul>
+          </nav>
         </div>
       </div>
     </section>
