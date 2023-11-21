4064d56f3bfc8f6129d885b9f9f05a44e1dac806
MatteoNocera <matteo_nocera@tiscali.it>
2023-11-21 17:40:08 +0100

    add state.js and modify navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,9 +1,10 @@
 <script>
+import { state } from '../state.js';
 export default {
     name: 'Navbar',
     data() {
         return {
-
+            state
         }
     }
 }
@@ -21,7 +22,7 @@ export default {
             <div class="collapse navbar-collapse" id="collapsibleNavId">
                 <ul class="navbar-nav me-auto mt-2 mt-lg-0">
                     <li class="nav-item">
-                        <router-link class="nav-link active" to="/" aria-current="page">Home <span
+                        <router-link class="nav-link" to="/" aria-current="page">Home <span
                                 class="visually-hidden">(current)</span></router-link>
                     </li>
                     <li class="nav-item">
@@ -30,8 +31,18 @@ export default {
                     <li class="nav-item">
                         <router-link class="nav-link" to="/about">About</router-link>
                     </li>
+                </ul>
+                <ul class="navbar-nav ms-auto mt-2 mt-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link" href="http://127.0.0.1:8000/admin">Admin</a>
+                        <a class="nav-link" :href="state.base_url + `/admin`">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                class="bi bi-box-arrow-in-right" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd"
+                                    d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0v-2z" />
+                                <path fill-rule="evenodd"
+                                    d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z" />
+                            </svg> Admin
+                        </a>
                     </li>
                 </ul>
 
---
+++ src/state.js
@@ -0,0 +1,6 @@
+import { reactive } from "vue";
+
+export const state = reactive({
+    base_url: 'http://127.0.0.1:8000',
+    project_api: '/api/projects',
+})
