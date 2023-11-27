abc34773e1b2fca22ec8db6e4951299f80d11fc5
MatteoNocera <134152768+MatteoNocera@users.noreply.github.com>
2023-11-27 17:15:12 +0100

    eliminate transition on my approach

--- src/view/HomeView.vue
+++ src/view/HomeView.vue
@@ -11,19 +11,12 @@ export default {
     },
     data() {
         return {
-            showText: false,
             state,
             loading: true,
             projects: [],
         };
     },
     methods: {
-        showApproachText() {
-            this.showText = true;
-        },
-        hideApproachText() {
-            this.showText = false;
-        },
 
         getProjects(url) {
             /* const url = state.base_url + state.project_api + '/latest'; */
@@ -49,9 +42,9 @@ export default {
 }
 </script>
 <template>
-    <div>
+    <div class="bg-secondary">
 
-        <div class="p-5 mb-4 jumbo shadow rounded-bottom-5">
+        <div class="p-5 jumbo shadow rounded-bottom-5">
             <div class="container-fluid py-5">
                 <h1 class="display-5 fw-bold">Welcome to My Portfolio</h1>
 
@@ -78,20 +71,21 @@ export default {
         <div class="container">
             <section class="d-flex justify-content-center my-4 text-center text-white">
 
-                <div class="col-6 bg-dark approach-card rounded-pill" @mouseover="showApproachText"
-                    @mouseleave="hideApproachText">
+                <div class="col-6 bg-dark approach-card rounded-pill">
                     <h2 class="">My Approach</h2>
-                    <p v-if="showText">
+
+                    <p>
                         I believe in the power of elegant code and user-centric design. My goal is to create solutions
                         that are not only technically robust but also a joy to interact with. Whether it's bringing a
                         beautiful UI to life or architecting a robust backend infrastructure, I approach each project
                         with enthusiasm and attention to detail.
                     </p>
+
                 </div>
             </section>
         </div>
 
-        <div class="my-4" v-if="!loading">
+        <div class="py-4" v-if="!loading">
             <section class="latest_project" v-if="projects">
                 <div class="container">
                     <h2>My Latest Projects</h2>
@@ -106,7 +100,7 @@ export default {
             </section>
         </div>
 
-        <div class="container my-3" v-else>
+        <div class="container py-3" v-else>
             <Loader></Loader>
         </div>
 
@@ -121,6 +115,6 @@ export default {
     cursor: pointer;
     padding: 15px;
     text-align: center;
-    transition: all 2s;
+
 }
 </style>
