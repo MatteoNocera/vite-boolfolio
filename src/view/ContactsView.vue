05b99b1851b6c81f8905d087c9d21b12779eb63b
MatteoNocera <134152768+MatteoNocera@users.noreply.github.com>
2023-11-28 00:44:00 +0100

    implement form

--- src/view/ContactsView.vue
+++ src/view/ContactsView.vue
@@ -17,6 +17,7 @@ export default {
             phone: '',
             message: '',
             errors: [],
+            success: null
         }
     },
     methods: {
@@ -35,7 +36,13 @@ export default {
 
                     const success = response.data.success;
 
-                    if (success) {
+                    if (!success) {
+
+                        console.log(response);
+                        console.log(response.data.errors);
+                        this.errors = response.data.errors;
+
+                    } else {
                         console.log(response);
                         console.log(response.data.message);
 
@@ -45,12 +52,6 @@ export default {
                         this.message = '';
 
                         this.success = response.data.message;
-
-                    } else {
-                        console.log(response);
-                        console.log(response.data.errors);
-                        this.errors = response.data.errors;
-
                     }
 
                     this.loading = false;
@@ -73,64 +74,115 @@ export default {
             <div class="container-fluid py-5">
                 <h1 class="display-5 fw-bold">Get in touch</h1>
                 <p class="col-md-8 fs-4">Find top news from top authors</p>
-                <button class="btn btn-primary btn-lg" type="button">Find out more</button>
+                <button class="btn btn-outline-secondary btn-lg" type="button">Find out more</button>
             </div>
         </div>
 
 
         <div class="container py-4">
-            <form action="" v-on:submit.prevent="sendForm()">
-                <div v-if="!loading">
-                    <div class="mb-3">
-                        <label for="name" class="form-label text-uppercase">Name</label>
-                        <input type="text" name="name" id="name" class="form-control" placeholder="YourName"
-                            aria-describedby="nameHelper" v-model="name">
-                        <small id="nameHelper" class="text-muted">Type your name</small>
 
+            <div class="row row-cols-1 row-cols-md-2 g-3">
+
+                <div class="col">
+                    <div v-if="success">
+                        <div class="alert alert-success" role="alert">
+                            <strong>{{ success }}</strong>
+                        </div>
                     </div>
 
-                    <div class="mb-3">
-                        <label for="phone" class="form-label  text-uppercase">phone</label>
-                        <input type="tel" name="phone" id="phone" class="form-control" placeholder="33333333333"
-                            aria-describedby="phoneHelper" v-model="phone">
-                        <small id="phoneHelper" class="text-muted">Type your phone</small>
+                    <form action="" v-on:submit.prevent="sendForm()">
+                        <div v-if="!loading">
+                            <div class="mb-3">
+                                <label for="name" class="form-label text-uppercase">Name</label>
+                                <input type="text" name="name" id="name" class="form-control" placeholder="YourName"
+                                    aria-describedby="nameHelper" v-model="name" :class="{ 'is-invalid': errors.name }">
+                                <small id="nameHelper" class="text-muted">Type your name</small>
 
-                    </div>
+                                <div class="alert alert-danger" role="alert" v-if="errors.name">
+                                    <strong>Errors</strong>
+                                    <ul>
+                                        <li v-for="message in errors.name">{{ message }}</li>
+                                    </ul>
+                                </div>
 
-                    <div class="mb-3">
-                        <label for="email" class="form-label text-uppercase">email</label>
-                        <input type="text" name="email" id="email" class="form-control" placeholder="name@example.com"
-                            aria-describedby="emailHelper" v-model="email">
-                        <small id="emailHelper" class="text-muted">Type your email</small>
 
-                    </div>
+                            </div>
 
-                    <div class="mb-3">
-                        <label for="message" class="form-label">Message</label>
-                        <textarea class="form-control" name="message" id="message" rows="3"
-                            placeholder="Your message here..." v-model="message"></textarea>
+                            <div class="mb-3">
+                                <label for="phone" class="form-label  text-uppercase">phone</label>
+                                <input type="tel" name="phone" id="phone" class="form-control" placeholder="33333333333"
+                                    aria-describedby="phoneHelper" v-model="phone" :class="{ 'is-invalid': errors.phone }">
+                                <small id="phoneHelper" class="text-muted">Type your phone</small>
 
-                    </div>
+                                <div class="alert alert-danger" role="alert" v-if="errors.phone">
+                                    <strong>Errors</strong>
+                                    <ul>
+                                        <li v-for="message in errors.phone">{{ message }}</li>
+                                    </ul>
+                                </div>
 
-                    <button type="submit" class="btn btn-primary mb-3">
-                        <span>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                class="bi bi-send" viewBox="0 0 16 16">
-                                <path
-                                    d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z" />
-                            </svg>
-                        </span>
-                        <span>
-                            Send
-                        </span>
+                            </div>
 
-                    </button>
+                            <div class="mb-3">
+                                <label for="email" class="form-label text-uppercase">email</label>
+                                <input type="text" name="email" id="email" class="form-control"
+                                    placeholder="name@example.com" aria-describedby="emailHelper" v-model="email"
+                                    :class="{ 'is-invalid': errors.email }">
+                                <small id="emailHelper" class="text-muted">Type your email</small>
+
+                                <div class="alert alert-danger" role="alert" v-if="errors.email">
+                                    <strong>Errors</strong>
+                                    <ul>
+                                        <li v-for="message in errors.email">{{ message }}</li>
+                                    </ul>
+                                </div>
+
+                            </div>
+
+                            <div class="mb-3">
+                                <label for="message" class="form-label">Message</label>
+                                <textarea class="form-control mb-4" name="message" id="message" rows="3"
+                                    placeholder="Your message here..." v-model="message"
+                                    :class="{ 'is-invalid': errors.message }"></textarea>
+
+                                <div class="alert alert-danger" role="alert" v-if="errors.message">
+                                    <strong>Errors</strong>
+                                    <ul>
+                                        <li v-for="message in errors.message">{{ message }}</li>
+                                    </ul>
+                                </div>
+
+                            </div>
+
+                            <button type="submit" class="btn btn-primary mb-3">
+                                <span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                        class="bi bi-send" viewBox="0 0 16 16">
+                                        <path
+                                            d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z" />
+                                    </svg>
+                                </span>
+                                <span>
+                                    Send
+                                </span>
+
+                            </button>
+                        </div>
+                        <div class="container py-3" v-else>
+                            <Sender></Sender>
+                        </div>
+
+                    </form>
                 </div>
-                <div class="container py-3" v-else>
-                    <Sender></Sender>
-                </div>
 
-            </form>
+                <div class="col d-flex flex-column justify-content-center gap-3 text-center">
+                    <h5>Contact me for any questions</h5>
+                    <img class="img-fluid rounded-2 shadow"
+                        src="https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExYjBzZGJxZzNtbjdwN24wdjIyOGNkMWk1ZmtjZDk2azNmNWo4Zmg2cSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/HzPtbOKyBoBFsK4hyc/giphy.gif"
+                        alt="">
+                </div>
+            </div>
+
         </div>
     </div>
 </template>
