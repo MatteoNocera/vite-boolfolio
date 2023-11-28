afb40b6aa7418f33d9c36311b9a96d9d5279fb46
MatteoNocera <134152768+MatteoNocera@users.noreply.github.com>
2023-11-28 01:36:59 +0100

    put style and img on assets folder

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import './style.scss'
+import './assets/style/style.scss';
 import '@fortawesome/fontawesome-free/css/all.css';
 import App from './App.vue'
 import { router } from './router.js'
